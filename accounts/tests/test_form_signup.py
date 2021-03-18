8c05132964c0131b34b0e938ba0ca9a59491184d
tyyuan <tyyuan@aibee.com>
2021-03-18 17:34:03 +0800

    add test signupform

--- accounts/tests/test_form_signup.py
+++ accounts/tests/test_form_signup.py
@@ -4,5 +4,14 @@ version:
 Author: TianyuYuan
 Date: 2021-03-18 16:49:56
 LastEditors: TianyuYuan
-LastEditTime: 2021-03-18 16:49:57
+LastEditTime: 2021-03-18 17:16:00
 '''
+from django.test import TestCase
+from .. forms import SignUpForm
+
+class SignUpFormTest(TestCase):
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['username', 'email', 'password1', 'password2']
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
